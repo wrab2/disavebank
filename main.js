9c85309c9530106d101bfbe2526e813da644a6fb
Wrab <namedj82@gmail.com>
2023-04-27 23:07:38 +0300

    initial commit, normal mode saves

---
+++ main.js
@@ -0,0 +1,30 @@
+const Col_names = ["  ","Normal","Hiker's dream","Extreme","Extreme Dream","Hard","Hard Dream","Absurd","Absurd Dream","Absurd Extreme","Absurd Extereme Dream","Absurd Hard","Absurd Hard Dream"]
+const Row_names = ["Normal","Easy","AAU","AAU Easy","NA","NA Eassy","AAU/NA","AAU/NA Easy"]
+const groups = ['pre-infinity','infinity','elementary','multiverse']
+//in format Col_namesRow_names
+const save_exists = ["NormalNormal",]
+loadedSaves = [{}]
+function howmuch() {return loadedSaves.length} 
+function bakktomenu(){currentpage=0;loadedSaves = [{}];app.$forceUpdate();}
+var currentpage=0
+var selectedmode='none'
+
+function loadMode(url) { //Shamelessly stolen from https://jmperezperez.com/blog/ondemand-javascript-lazy-loading-stubs/
+  url = "./saves/".concat(url).concat('.js')
+  var script = document.createElement('script');
+  script.setAttribute('src', url);
+  script.setAttribute('type', 'text/javascript');
+  var loaded = false;
+  var loadFunction = function () { if (loaded) return; loaded = true;};
+  script.onload = loadFunction;
+  script.onreadystatechange = loadFunction;
+  document.getElementsByTagName("head")[0].appendChild(script);
+  currentpage=1;
+  selectedmode=url
+};
+
+var app = new Vue({
+  el: "#app",
+  data: {
+  },
+})
---
+++ readme.md
@@ -0,0 +1,25 @@
+## Don't add every single save out there, only useful ones
+Meaning that if you have save with for example 10 TME there's no need to have a save with 11 TME, 12 TME, 13 TME etc, they all achievable in a few seconds from 10 TME. Same goes for rockets, endorsements and time cubes. 
+
+---
+
+## How to add saves
+put your saves in  
+`/saves/<ModeName>.js`\
+if you're adding a save to the empty mode combination add it to `save_exists` const as well\
+Example of a file with saves:
+```
+loadedSaves = [
+  {
+  	string: 'save goes here',
+	name: 'Displayed name of a save',
+	group: 'pre-infinity',
+	author:  'who made this save',
+  },
+  {
+	  ...
+  },
+]
+app.$forceUpdate()
+```
+Available groups are `'pre-infinity',   'infinity',   'elementary',   'multiverse', 'endgame'`. Group only affects color of the text.
