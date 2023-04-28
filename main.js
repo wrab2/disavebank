4ff6a3d07baa15ac54bbc013e6465d0044ed6d66
Wrab <namedj82@gmail.com>
2023-04-28 12:17:22 +0300

    cleaning

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-const Col_names = ["  ","Normal","Hiker's dream","Extreme","Extreme Dream","Hard","Hard Dream","Absurd","Absurd Dream","Absurd Extreme","Absurd Extereme Dream","Absurd Hard","Absurd Hard Dream"]
+const Col_names = ["","Normal","Hiker's dream","Extreme","Extreme Dream","Hard","Hard Dream","Absurd","Absurd Dream","Absurd Extreme","Absurd Extereme Dream","Absurd Hard","Absurd Hard Dream"]
 const Row_names = ["Normal","Easy","AAU","AAU Easy","NA","NA Eassy","AAU NA","AAU NA Easy"]
-const groups = ['pre-infinity','infinity','elementary','multiverse']
 
 
 const save_exists = [ //in format Col_namesRow_names
@@ -14,12 +13,11 @@ const save_exists = [ //in format Col_namesRow_names
 "HardNA",
 "Absurd HardNA",
 ]
+
 loadedSaves = [{}]
 function howmuch() {return loadedSaves.length} 
 function bakktomenu(){currentpage=0;loadedSaves = [{}];app.$forceUpdate();}
 var currentpage=0
-var selectedmode='none'
-
 function loadMode(url) { //Shamelessly stolen from https://jmperezperez.com/blog/ondemand-javascript-lazy-loading-stubs/
   url = "./saves/".concat(url).concat('.js')
   var script = document.createElement('script');
@@ -31,9 +29,7 @@ function loadMode(url) { //Shamelessly stolen from https://jmperezperez.com/blog
   script.onreadystatechange = loadFunction;
   document.getElementsByTagName("head")[0].appendChild(script);
   currentpage=1;
-  selectedmode=url
 };
-
 var app = new Vue({
   el: "#app",
   data: {
