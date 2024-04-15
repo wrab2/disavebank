e2791768f939ec5297156ee29965ae80e525ca26
Wrab <namedj82@gmail.com>
2024-04-15 18:41:18 +0300

    QOL and a few AAU easy saves

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const Row_names = ["Normal","Easy","AAU","AAU Easy","NA","NA Easy","AAU NA","AAU
 
 const save_exists = [ //in format Col_namesRow_names
 "NormalNormal",
+"NormalAAU Easy",
 "Hiker's dreamNormal",
 "ExtremeNormal",
 "Extreme DreamNormal",
@@ -15,11 +16,10 @@ const save_exists = [ //in format Col_namesRow_names
 "Hard DreamNA",
 ]
 
-loadedSaves = [{}]
-function howmuch() {return loadedSaves.length} 
-function bakktomenu(){currentpage=0;loadedSaves = [{}];app.$forceUpdate();}
-var currentpage=0
-function loadMode(url) { //Shamelessly stolen from https://jmperezperez.com/blog/ondemand-javascript-lazy-loading-stubs/
+let loadedSaves = [{}]
+function backtomenu(){currentpage=0;loadedSaves = [{}];app.$forceUpdate();}
+let currentpage = 0
+function loadMode(url) { //taken from https://jmperezperez.com/blog/ondemand-javascript-lazy-loading-stubs/
   url = "./saves/".concat(url).concat('.js')
   var script = document.createElement('script');
   script.setAttribute('src', url);
@@ -29,7 +29,7 @@ function loadMode(url) { //Shamelessly stolen from https://jmperezperez.com/blog
   script.onload = loadFunction;
   script.onreadystatechange = loadFunction;
   document.getElementsByTagName("head")[0].appendChild(script);
-  currentpage=1;
+  currentpage = 1;
 };
 var app = new Vue({
   el: "#app",
